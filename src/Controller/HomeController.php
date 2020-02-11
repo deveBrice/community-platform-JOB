ec311004af598075837a192881c4f41a3a45a631
Omar ATARI <atari.omar@gmail.com>
2020-02-11 01:37:04 +0100

    updated security.yml and home controller
    added redirection to login when the visitor is not connected

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,20 @@
 <?php
   namespace App\Controller;
- 
+
   use App\Entity\User;
   use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
   use Symfony\Component\Routing\Annotation\Route;
 
   class HomeController extends AbstractController {
-    
+
     /**
      * @Route("/", name="home")
      */
     public function indexAction()
     {
-        return $this->render('home.html.twig');
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        return $this->render('home.html.twig',[
+            'current_menu' => 'home'
+        ]);
     }
   }
-?>
