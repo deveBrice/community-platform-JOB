0646de71bf6b8643bcc48315e9d263b226a66e5a
Omar ATARI <atari.omar@gmail.com>
2020-02-24 14:36:49 +0100

    added faker bundle
    added faker bundle with UserFixtures to populate database

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        for($i=0; $i<10; $i++){
+            $user = new User();
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
+            $user->setEmail($faker->email);
+            $user->setPassword($this->encoder->encodePassword($user, 'cpjob'));
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
