711ba4154a58c4a0b5e3006c12d62b709fd9455e
quevenne <j-quevenne@ecole-ipssi.net>
2020-02-25 11:08:11 +0100

    added entity Content

---
+++ src/Entity/Content.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContentRepository")
+ */
+class Content
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descritption;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\User", inversedBy="content", cascade={"persist", "remove"})
+     */
+    private $author;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getDescritption(): ?string
+    {
+        return $this->descritption;
+    }
+
+    public function setDescritption(string $descritption): self
+    {
+        $this->descritption = $descritption;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+}
