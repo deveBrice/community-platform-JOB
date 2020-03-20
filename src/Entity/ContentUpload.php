025536b15881362c5ae24773eb4950ae9a187434
Omar ATARI <atari.omar@gmail.com>
2020-03-20 05:56:04 +0100

    updated content upload entity

--- src/Entity/ContentUpload.php
+++ src/Entity/ContentUpload.php
@@ -17,20 +17,65 @@ class ContentUpload
     private $id;
 
     /**
-     * @ORM\OneToOne(targetEntity="App\Entity\Content", cascade={"persist", "remove"})
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="blob")
+     */
+    private $file;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Content", inversedBy="contentUploads")
      * @ORM\JoinColumn(nullable=false)
      */
     private $id_content;
 
-    /**
-     * @ORM\OneToOne(targetEntity="App\Entity\UploadedFile", cascade={"persist", "remove"})
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $id_upload;
-
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    public function setFile($file): self
+    {
+        $this->file = $file;
+
+        return $this;
     }
 
     public function getIdContent(): ?Content
@@ -38,21 +83,9 @@ class ContentUpload
         return $this->id_content;
     }
 
-    public function setIdContent(Content $id_content): self
+    public function setIdContent(?Content $id_content): self
     {
         $this->id_content = $id_content;
-
-        return $this;
-    }
-
-    public function getIdUpload(): ?UploadedFile
-    {
-        return $this->id_upload;
-    }
-
-    public function setIdUpload(UploadedFile $id_upload): self
-    {
-        $this->id_upload = $id_upload;
 
         return $this;
     }
